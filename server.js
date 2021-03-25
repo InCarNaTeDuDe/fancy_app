f17307f271d183a3dd82c8eaad633f05c9d10f08
bharathmaska <bharathmaska163@gmail.com>
2021-03-25 17:36:39 +0530

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http"),
   //   querystring = require("querystring"),
   url = require("url"),
+  path = require("path"),
   fs = require("fs");
 
 const PORT = process.env.PORT || 8080;
@@ -19,11 +20,25 @@ http
 
     var route = req.url;
     if (route === "/send") {
-      fs.appendFile('helloworld.txt', 'Thankyou!\n', function (err) {
+      fs.appendFile("helloworld.txt", "Thankyou!\n", function (err) {
         if (err) return console.log(err);
       });
       res.write("<script>alert('Thank you')</script>"); //write a response
       res.end(); //end the response
+    }
+
+    if (route === "/helloworld") {
+      var filePath = path.join(__dirname, "helloworld.txt");
+      var stat = fs.statSync(filePath);
+
+      res.writeHead(200, {
+        "Content-Type": "plain/txt",
+        "Content-Length": stat.size,
+      });
+
+      var readStream = fs.createReadStream(filePath);
+      // We replaced all the event handlers with a simple call to readStream.pipe()
+      readStream.pipe(res);
     }
 
     const queryObject = url.parse(req.url, true).query;
